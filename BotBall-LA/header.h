a2795da59682b5329d44e91b7f204fd4384b29ff
Tony Xue <xuezxbb@gmail.com>
2014-07-03 18:36:24 +0800

    First rough draft for the new design

--- BotBall-LA/header.h
+++ BotBall-LA/header.h
@@ -1,13 +1,14 @@
 #define pi 3.1415926
 
-#define liftingMotorPort 0
+#define liftingMotorPort1 0
+#define liftingMotorPort2 3
 #define catchingServoPort 3
 #define lightSensorPortNum 0
 #define etSensorPort 4
 #define sensorLiftingServo 2
-#define blackLineSensorPort 1
+#define blackLineSensorPort 6
 #define orangeChan 0
 #define yellowChan 1
 
-#define closeHand set_servo_position(catchingServoPort,1000)
+#define closeHand set_servo_position(catchingServoPort,1400)
 #define openHand set_servo_position(catchingServoPort,0)
--- BotBall-LA/main.c
+++ BotBall-LA/main.c
@@ -24,9 +24,10 @@ void servoInit()
 }
 void motorInit()
 {
-	motor(liftingMotorPort,100);
-	msleep(7000);
-	off(liftingMotorPort);
+	motor(liftingMotorPort1,100);
+	motor(liftingMotorPort2,100);
+	msleep(6000);
+	ao();
 	printf("Motor initialized!\n");
 }
 int blackLine()
@@ -86,49 +87,56 @@ int identColor()
 	{
 		camera_update();
 		if (get_object_count(orangeChan) == 0 && get_object_count(yellowChan) == 0) return -1;
-		if (get_object_count(orangeChan) > 0) return 0;
-		if (get_object_count(yellowChan) > 0) return 1;
+		if (get_object_area(orangeChan,0) >= 20) return 0;
+		if (get_object_area(yellowChan,0) >= 20) return 1;
 	}
 	camera_close();
 }
 void goToHangerRack();
 void putHangers();
-void blueHangers();
-void orangeCube();
-void yellowCube();
 
 int main()
 {
 	int cTime=0,sTime,foundOrange=0;
 	printf("Start!\n");
-	printf("Wait for light!\n");
+	//printf("Wait for light!\n");
 	//lightDetection();
+
+	set_a_button_text("Start!\n");
+	while (!a_button_clicked()){}
+
 	shut_down_in(120);
 
 	servoInit(); // Supply power to all the servos
 	motorInit(); // set the motor position
-	create_connect();// connect the create
-	create_full();// FULL mode, the create will execute commands under any circumstance
+	create_connect(); // connect the create
+	create_full(); // FULL mode, the create will execute commands under any circumstance
 	printf("Create connected!\n Battery: %d\n",get_create_battery_charge());
-	msleep(1000);
+	create_drive_straight(100); // avoid scratching with the tube
+	msleep(300);
+	turnLeft(90); // adjust heading in startup area
 	
-	turnLeft(90);// adjust heading in startup area
 	goToHangerRack(); // create move to the hanger rack
 	putHangers(); // the "hand" put hangers on the rack
 	
 	create_drive_straight(-200); // drive away from hanger rack
-	msleep(1500);
+	msleep(1000);
 	create_stop();
 
-	turnRight(45);
+	turnRight(35);
 	create_drive_straight(200);
 	msleep(500);
 	create_stop();
 
-	openHand; // Open the hand
+	motor(liftingMotorPort1,-100);
+	motor(liftingMotorPort2,-100);
+	msleep(2000);
+	ao();
+
+	//openHand; // Open the hand
 	sTime = seconds();
-	create_spin_CW(200);
-	while (cTime - sTime <= 5.0)
+	create_spin_CW(50);
+	while (cTime - sTime <= 20.0)
 	{
 		cTime = seconds();
 		if (identColor() == 0)
@@ -139,19 +147,18 @@ int main()
 	}
 	if (foundOrange = 0)
 	{
-		motor(liftingMotorPort,-100);
-		msleep(500);
-		off(liftingMotorPort);
-
+		motor(liftingMotorPort1,-100);
+		motor(liftingMotorPort2,-100);
+		msleep(300);
+		ao();
 		sTime = seconds();
 		cTime =  seconds();
-		create_spin_CCW(200);
-		while (cTime - sTime <= 5.0)
+		create_spin_CCW(50);
+		while (cTime - sTime <= 20.0)
 		{
 			cTime = seconds();
 			if (identColor == 0)
 			{
-				foundOrange = 1;
 				break;
 			}
 		}
@@ -160,18 +167,19 @@ int main()
 	set_servo_position(catchingServoPort,500);
 	sTime = seconds();
 	create_drive_straight(200);
-	while (analog_et(etSensorPort<500)){}
+	while (analog_et(etSensorPort < 500)){}
 	create_stop();
 	cTime = seconds();
 	closeHand; // Close the hand
-	motor(liftingMotorPort,100);
+	motor(liftingMotorPort1,100);
+	motor(liftingMotorPort2,100);
 	msleep(200);
-	off(liftingMotorPort);
+	ao();
 	create_drive_straight(-200);
 	msleep(cTime - sTime);
 	create_stop();
 	create_spin_CCW(200);
-	msleep(cTime - sTime);
+	msleep((cTime - sTime)/4);
 	create_stop();
 	create_drive_straight(200);
 	while (!blackLine()){}
@@ -187,10 +195,11 @@ int main()
 	create_drive_straight(-200);
 	msleep(1000);
 	create_stop();
-	motor(liftingMotorPort,-100);
+	motor(liftingMotorPort1,-100);
+	motor(liftingMotorPort2,-100);
 	msleep(1000);
-	off(liftingMotorPort);
-	set_servo_position(catchingServoPort,0);
+	ao();
+	openHand;
 
 	return 0;
 }
@@ -199,8 +208,12 @@ void goToHangerRack()
 	printf("Going to the hanger rack!\n");
 	create_drive_straight(200);
 	while(!blackLine()){}
+	create_drive_straight(-200);
+	msleep(600);
 	turnLeft(90);
-	goAlongLine(5.6);
+	//goAlongLine(5.6);
+	create_drive_straight(200);
+	msleep(2000);
 	turnRight(90);
 	create_stop();
 }
@@ -209,31 +222,8 @@ void putHangers()
 	printf("Start putting hangers!\n");
 	//put the hangers on to the PVC
 	create_drive_straight(200);
-	msleep(450);
+	msleep(1200);
 	create_stop();
 	openHand; // open the "hand" to put the hangers
 	msleep(500);
-	motor(liftingMotorPort,-100);
-	msleep(280);
-	off(liftingMotorPort);
-}
-void cube()
-{
-	int yellowCount=0,colorResult;
-	printf("Start getting orange cube!\n");
-	//the variable oFlag, which is defined as a global variable, is used in this function as an indicator for whether the robot has successfully gotten the orange cube after several trials.
-	double cTime,sTime;// cTime stands for Current Time, sTime stands for Start Time
-	goAlongLine(1.5);
-	create_stop();
-	//lift the arm
-	sTime = seconds();
-	create_spin_CCW(200);
-	while (yellowCount != 2)
-	{
-		colorResult = identColor();
-		if (colorResult == 0) break;
-		if (colorResult == 1) yellowCount++;
-	}
-	if (yellowCount == 0) // No yellow cube on the upper shelf
-	{}
 }
